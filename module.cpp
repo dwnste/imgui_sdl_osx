d7ed064145cfa0b2a200a89f1c00f42cb5f43b02
downstate <dwnste@gmail.com>
2018-03-02 20:11:04 +0300

    Add module

---
+++ module.cpp
@@ -0,0 +1,46 @@
+//
+// libembryo
+//
+// Copyright Stanley Cen 2013
+// Released under the MIT License
+//
+
+#include "module.hpp"
+
+std::string getFileNameFromPath(const std::string& path)
+{
+    int pos = (int)path.length();
+    while (pos && path[pos] != '/') --pos;
+    return path.substr(pos + 1);
+}
+namespace embryo {
+    module::module(const std::string& name) : m_start(0), m_size(0)
+    {
+        bool found = false;
+        unsigned int imageCount = _dyld_image_count();
+        for (int i = 0; i < imageCount; i++)
+        {
+            mach_header *header = (mach_header *)_dyld_get_image_header(i);
+            
+            const char *imageName = _dyld_get_image_name(i);
+            if (!imageName) continue;
+            
+            std::string shortName = getFileNameFromPath(std::string(imageName));
+            if (shortName != name) continue;
+            
+            struct stat sb;
+            if (stat(imageName, &sb))
+            {
+                break;
+            }
+            found = true;
+            m_start = (void *)header;
+            m_size = (unsigned int)sb.st_size;
+            m_header = header;
+            m_name = shortName;
+            m_path = std::string(imageName);
+            m_handle = dlopen(imageName, RTLD_GLOBAL | RTLD_LAZY);
+            break;
+        }
+    }
+}
---
+++ module.hpp
@@ -0,0 +1,48 @@
+//
+// libembryo
+//
+// Copyright Stanley Cen 2013
+// Released under the MIT License
+//
+
+#ifndef libembryo_module_h
+#define libembryo_module_h
+
+#include <string>
+
+#include <sys/types.h>
+#include <mach/error.h>
+#include <mach/vm_types.h>
+#include <mach-o/dyld.h>
+#include <mach-o/getsect.h>
+#include <mach/mach.h>
+#include <sys/stat.h>
+#include <dlfcn.h>
+
+namespace embryo {
+    class module
+    {
+    public:
+        module(const std::string &name);
+        
+        void *start() const { return m_start; }
+        unsigned int size() const { return m_size; }
+        std::string name() const { return m_name; }
+        std::string path() const { return m_path; }
+        mach_header *machHeader() const { return m_header; }
+        void *handle() const { return m_handle; }
+        template<typename Fn>
+        Fn getSymbol(const char *name) const
+        {
+            return (Fn)dlsym(handle(), name);
+        }
+    private:
+        void *m_start;
+        unsigned int m_size;
+        std::string m_name;
+        std::string m_path;
+        mach_header *m_header;
+        void *m_handle;
+    };
+}
+#endif
